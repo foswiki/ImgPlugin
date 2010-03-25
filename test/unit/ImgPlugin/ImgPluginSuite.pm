7e25bcf89d545d0f9deb271ebd3f67a076bde61a
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-03-25 14:51:12 +0000

    Item8765: Replace use base with use; our @ISA;
    git-svn-id: http://svn.foswiki.org/trunk/ImgPlugin@6912 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- test/unit/ImgPlugin/ImgPluginSuite.pm
+++ test/unit/ImgPlugin/ImgPluginSuite.pm
@@ -1,6 +1,7 @@
 package ImgPluginSuite;
 
-use base qw(Unit::TestSuite);
+use Unit::TestSuite;
+our @ISA = qw( Unit::TestSuite );
 
 sub name { 'ImgPluginSuite' };
 
--- test/unit/ImgPlugin/ImgPluginTests.pm
+++ test/unit/ImgPlugin/ImgPluginTests.pm
@@ -2,7 +2,8 @@ use strict;
 
 package ImgPluginTests;
 
-use base qw(FoswikiTestCase);
+use FoswikiTestCase;
+our @ISA = qw( FoswikiTestCase );
 
 use strict;
 use TWiki;
